9ee4d0a8bcefbe02214e6d119f15ef93b2690c4c
chansooligans <cs2737@nyu.edu>
2023-05-02 23:08:32 -0400

    update readme

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # tailoredscoop  
 
-## Case Team  
-Division(s): Other  
-Bureau(s):  
-Assistant Attorney General(s):  
-Research Staff(s): Chansoo Song    
-
-## Documentation
-
-See https://research.pages.ag.ny.gov/tailoredscoop/
+Automated customized daily news letter service.
--- scripts/top_stories.py
+++ scripts/top_stories.py
@@ -1,50 +1,32 @@
 # %%
-from IPython import get_ipython
-if get_ipython() is not None:
-    get_ipython().run_line_magic("load_ext", "autoreload")
-    get_ipython().run_line_magic("autoreload", "2")
-
-import openai
-import tiktoken
 from tailoredscoop.news import api
 from tailoredscoop import config
 from tailoredscoop.documents import summarize
+import openai
+from django.core.mail import send_mail
 
-# %%
+email = "chansoosong@gmail.com" # email here
+keywords = "" # keywords here
+
 secrets = config.setup()
+openai.api_key = secrets["openai"]["api_key"]
 news_downloader = api.NewsAPI(
     api_key = secrets["newsapi"]["api_key"]
 )
 
-articles = news_downloader.get_top_news()
+if keywords == "":
+    articles = news_downloader.get_top_news()
+else:
+    articles = news_downloader.query_news_by_topic(keywords)
 res = news_downloader.process(articles, summarizer=summarize.summarizer)
 
-# %%
-today_news = "; ".join(res.values())
-sources = "- "+"\n- ".join(res.keys())
+summary = summarize.get_openai_summary(res)
+print(summary)
 
-messages = [
-    {"role": "system", "content": "You are an energetic, fun, and witty daily news blogger."},
-    {"role": "user", "content": "Please create a morning newsletter based on today's stories, which are stories from today's news."},
-    {"role": "user", "content": "Separate different topics using different paragraphs. If the story is not too serious, feel free to include emojis and puns. Each bullet point should contain at least three sentences."},
-    {"role": "user", "content": f"Today's stories are: {today_news}. The newsletter:"},
-]
-
-# %%
-num_tokens = summarize.num_tokens_from_messages(messages, model="gpt-3.5-turbo")
-
-# %%
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages = messages,
-    temperature=0.8,
-    max_tokens=4096-num_tokens
+send_mail(
+    subject="Today's Tailored Scoop",
+    message=summary,
+    from_email="chansoosong@gmail.com",
+    recipient_list=["chansoosong@gmail.com"],
+    fail_silently=False,
 )
-
-# %%
-print(response["choices"][0]["message"]["content"])
-
-# %%
-print(sources)
-
-# %%
