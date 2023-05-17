a8547d6197c0fce93aeaaf0dd94d25abb1817ad7
chansooligans <cs2737@nyu.edu>
2023-05-16 23:00:34 -0400

    testing top stories

--- tailoredscoop/news/newsapi_with_google_kw.py
+++ tailoredscoop/news/newsapi_with_google_kw.py
@@ -31,10 +31,10 @@ class DownloadArticle:
         }
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(url, headers=headers, timeout=20) as response:
+                async with session.get(url, headers=headers, timeout=60) as response:
                     return await response.text()
         except asyncio.TimeoutError:
-            print(f"Request timed out after 20 seconds: {url}")
+            print(f"Request timed out after 60 seconds: {url}")
             raise
 
     async def extract_article_content(self, url: str) -> Optional[str]:
@@ -239,7 +239,7 @@ class NewsAPI(SetupMongoDB, DocumentProcessor, DownloadArticle, GoogNewsReFormat
             print(f"Query already requested: {url_hash}")
             return list(db.articles.find({"query_id": url_hash}).sort("created_at", -1))
 
-        articles = feedparser.parse(url).entries[:15]
+        articles = feedparser.parse(url).entries[:10]
         articles = await self.reformat_google(articles)
         if articles:
             await self.download(articles, url_hash, db)
@@ -274,8 +274,9 @@ class NewsAPI(SetupMongoDB, DocumentProcessor, DownloadArticle, GoogNewsReFormat
         results = []
         used_q = ""
         for query in q.split(","):
+            query = query.lower()
             if query in GOOGLE_TOPICS.keys():
-                url = f"""https://news.google.com/rss/topics/{GOOGLE_TOPICS[query.lower()]}"""
+                url = f"""https://news.google.com/rss/topics/{GOOGLE_TOPICS[query]}"""
             else:
                 url = f"""https://news.google.com/rss/search?q="{quote(query)}"%20when%3A1d"""
 
@@ -285,8 +286,8 @@ class NewsAPI(SetupMongoDB, DocumentProcessor, DownloadArticle, GoogNewsReFormat
                 new_q = get_similar_keywords_from_gpt(query)
                 if len(new_q) == 0:
                     return [], q
-                query = "OR".join([f'"{x.strip()}"' for x in new_q.split(",")])
-                url = f"https://news.google.com/rss/search?q={quote(query)}%20when%3A1d"
+                new_query = "OR".join([f'"{x.strip()}"' for x in new_q.split(",")])
+                url = f"https://news.google.com/rss/search?q={quote(new_query)}%20when%3A1d"
                 print("query url: ", url)
                 articles = await self.request_google(db=db, url=url)
                 used_q = used_q + ", " + new_q
