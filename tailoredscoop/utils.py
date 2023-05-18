151d7e2c7b454e5f27a5929d78b60c9a4d69df9f
chansooligans <cs2737@nyu.edu>
2023-05-18 18:48:26 -0400

    save log file

--- tailoredscoop/utils.py
+++ tailoredscoop/utils.py
@@ -12,10 +12,18 @@ class Logger:
         if logger.hasHandlers():
             return
         else:
+            fh = logging.FileHandler("./logs/log.log")
+            fh.setLevel(logging.DEBUG)
+
             ch = logging.StreamHandler()
             ch.setLevel(logging.DEBUG)
+
             formatter = logging.Formatter(
                 "\n%(asctime)s - %(name)s - %(levelname)s - %(funcName)20s() | %(message)s"
             )
+
+            fh.setFormatter(formatter)
             ch.setFormatter(formatter)
+
+            logger.addHandler(fh)
             logger.addHandler(ch)
