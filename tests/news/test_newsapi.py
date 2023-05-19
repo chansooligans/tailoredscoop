e53d056677db5fe9be900f32f0777f45a35295d9
chansooligans <cs2737@nyu.edu>
2023-05-19 18:50:01 -0400

    remove "true url" func and google redirect class

--- tests/news/test_newsapi.py
+++ tests/news/test_newsapi.py
@@ -1,3 +1,5 @@
+import datetime
+
 import mongomock
 import pytest
 from aiohttp.test_utils import make_mocked_coro
@@ -33,22 +35,48 @@ def content():
 def mock_articles():
     return [
         {
+            "link": "news.google/rss/1",
             "url": "mock://example.com/article1",
-            "publishedAt": "2023-01-01T00:00:00Z",
-            "source": "Example News",
+            "published": "Fri, 19 May 2023 21:40:11 GMT",
+            "source": {"title": "Example News"},
             "title": "Example Article",
-            "description": "Description for Example Article",
-            "author": "Example Author",
             "content": "Pod of Dolphins Spotted off Miami Beach",
         },
         {
+            "link": "news.google/rss/2",
             "url": "mock://example.com/article2",
-            "publishedAt": "2023-01-02T00:00:00Z",
+            "published": "Fri, 19 May 2023 21:40:11 GMT",
+            "source": {"title": "Example News"},
+            "title": "Another Article",
+            "content": "Lions in the African Savannah",
+        },
+    ]
+
+
+@pytest.fixture
+def expected_processed_articles():
+    return [
+        {
+            "url": "mock://example.com/article1",
+            "link": "news.google/rss/1",
+            "published": datetime.datetime(2023, 5, 19, 21, 40, 11),
+            "source": "Example News",
+            "title": "Example Article",
+            "content": "Pod of Dolphins Spotted off Miami Beach",
+            "created_at": datetime.datetime(2023, 5, 19, 17, 42, 44, 612498),
+            "query_id": "sample_url_hash",
+            "rank": 0,
+        },
+        {
+            "url": "mock://example.com/article2",
+            "link": "news.google/rss/2",
+            "published": datetime.datetime(2023, 5, 19, 21, 40, 11),
             "source": "Example News",
             "title": "Another Article",
-            "description": "Description for Another Article",
-            "author": "Another Author",
             "content": "Lions in the African Savannah",
+            "created_at": datetime.datetime(2023, 5, 19, 17, 42, 44, 612872),
+            "query_id": "sample_url_hash",
+            "rank": 1,
         },
     ]
 
@@ -58,14 +86,15 @@ def news_api() -> NewsAPI:
     return NewsAPI(api_key="")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def db() -> mongomock.database.Database:
     client = mongomock.MongoClient()
     db = client.news_test
     entries = [
         {
+            "link": "news.google/rss/3",
             "url": "mock://example.com/article3",
-            "publishedAt": "2023-01-02T00:00:00Z",
+            "published": "2023-01-02T00:00:00Z",
             "source": "Example News",
             "title": "Another Article",
             "description": "Description for Another Article",
@@ -83,10 +112,17 @@ def db() -> mongomock.database.Database:
 
 @pytest.mark.asyncio
 async def test_download(
-    mocker, content, mock_articles, news_api: NewsAPI, db: mongomock.database.Database
+    mocker,
+    content,
+    mock_articles,
+    expected_processed_articles,
+    news_api: NewsAPI,
+    db: mongomock.database.Database,
 ):
     resp = MockResponse(content[0], 200)
+    resp.url = "mock://example.com/article1"
     resp2 = MockResponse(content[1], 200)
+    resp2.url = "mock://example.com/article2"
     mocker.patch("aiohttp.ClientSession.get", side_effect=[resp, resp2])
     url_hash = "sample_url_hash"
 
@@ -94,19 +130,16 @@ async def test_download(
     assert len(results) == len(mock_articles)
 
     results = sorted(results, key=lambda d: d["url"])
+
     for key in [
         "url",
-        "publishedAt",
+        "published",
         "source",
         "title",
-        "description",
-        "author",
         "content",
     ]:
-        assert all(
-            result[key] == article[key]
-            for article, result in zip(mock_articles, results)
-        )
+        for article, result in zip(expected_processed_articles, results):
+            assert result[key] == article[key]
 
     stored_article = db.articles.find_one({"url": mock_articles[0]["url"]})
     assert stored_article is not None
@@ -114,27 +147,41 @@ async def test_download(
 
 
 @pytest.mark.parametrize(
-    ("url,expected"),
+    ("link,expected"),
     [
-        ("www.cantfindthis.com", 0),
-        ("mock://example.com/article3", 1),
+        ("www.cantfindthis.com", None),
+        (
+            "news.google/rss/3",
+            {
+                "link": "news.google/rss/3",
+                "url": "mock://example.com/article3",
+                "published": "2023-01-02T00:00:00Z",
+                "source": "Example News",
+                "title": "Another Article",
+                "description": "Description for Another Article",
+                "author": "Another Author",
+                "content": "This was already in the database",
+            },
+        ),
     ],
 )
 @pytest.mark.asyncio
-async def test_check_db_for_article(url, expected, news_api, db):
-    article_check = news_api.check_db_for_article(url=url, db=db)
-    assert len(article_check) == expected
+async def test_check_db_for_article(link, expected, news_api, db):
+    article_check = news_api.check_db_for_article(link=link, db=db)
+    assert article_check == expected
 
 
 @pytest.mark.asyncio
 async def test_process_article(mocker, content, mock_articles, news_api, db):
     resp = MockResponse(content[0], 200)
+    resp.url = "mock://example.com/article1"
     resp2 = MockResponse(content[1], 200)
+    resp2.url = "mock://example.com/article2"
     mocker.patch("aiohttp.ClientSession.get", side_effect=[resp, resp2])
 
     await news_api.process_article(
-        news_article=mock_articles[0], url_hash="url_hash", db=db, rank=100
+        article=mock_articles[0], url_hash="url_hash", db=db, rank=100
     )
 
-    stored_article = db.articles.find_one({"url": mock_articles[0]["url"]})
+    stored_article = db.articles.find_one({"link": mock_articles[0]["link"]})
     assert stored_article["rank"] == 100
